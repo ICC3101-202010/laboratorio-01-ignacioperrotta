0ff6a4276b399e14839038319068134ee3fa219c
John Doe <ijperrotta@miuandes.cl>
2020-03-24 12:12:33 -0300

    lab commit

---
+++ Laboratorio1/Laboratorio1/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio
+
+{
+    public class Person
+    {
+        public string name;
+        public string lastname;
+        public int lanzar()
+        {
+            Random p1 = new Random();
+            int lanzar = p1.Next(3);
+            return lanzar;
+        }
+
+
+        public Person()
+        {
+            this.name = "Bob";
+            this.lastname = "Kunga";
+        }
+        public string Getname()
+        {
+            return name;
+        }
+        public string Getlastname()
+        {
+            return lastname;
+        }
+
+    }
+}
---
+++ Laboratorio1/Laboratorio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            Console.WriteLine(p.lanzar());
+            Person p2 = new Person();
+            Console.WriteLine(p2.Getname() + " " + p2.Getlastname());
+        }
+    }
+}
